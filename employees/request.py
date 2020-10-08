6c236c5ded04b62ff68e5bc155a3621c980eefb1
LukeZvada <zvadamusic@gmail.com>
2020-10-08 12:15:09 -0700

    code cleanup

--- employees/request.py
+++ employees/request.py
@@ -41,25 +41,18 @@ def get_single_employee(id):
     return requested_employee
 
 def delete_employee(id):
-    # Initial -1 value for animal index, in case one isn't found
     employee_index = -1
-
-    # Iterate the ANIMALS list, but use enumerate() so that you
-    # can access the index value of each item
     for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            # Found the animal. Store the current index.
-            employee_index = index
 
-    # If the animal was found, use pop(int) to remove it from list
+        if employee["id"] == id:
+            employee_index = index
+            
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
 
 def update_employee(id, new_employee):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
     for index, employee in enumerate(EMPLOYEES):
+        
         if employee["id"] == id:
-            # Found the animal. Update the value.
             EMPLOYEES[index] = new_employee
             break
